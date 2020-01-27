aae1e43ebad3d5c44f788afbfd086ee8789674e8
Lalithkumaraditya <lalithkumaraditya@gmail.com>
2020-01-27 11:24:30 +0530

    Updates

---
+++ precache-manifest.5194d38075710b18d1687bf87035a813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e8071f18dd3572a209c863a2bf2f52",
+    "url": "/Monster_React/index.html"
+  },
+  {
+    "revision": "44d5ff41526fad1d9ef1",
+    "url": "/Monster_React/static/css/main.e9738734.chunk.css"
+  },
+  {
+    "revision": "be33b2956ab92704ea98",
+    "url": "/Monster_React/static/js/2.b634fb3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster_React/static/js/2.b634fb3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44d5ff41526fad1d9ef1",
+    "url": "/Monster_React/static/js/main.438ca435.chunk.js"
+  },
+  {
+    "revision": "f63e39a17f9ccda9ea04",
+    "url": "/Monster_React/static/js/runtime-main.01c0feae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/live-monstors/precache-manifest.773f3be897e8443fefa0eef827a508d1.js"
+  "/Monster_React/precache-manifest.5194d38075710b18d1687bf87035a813.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/live-monstors/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Monster_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
